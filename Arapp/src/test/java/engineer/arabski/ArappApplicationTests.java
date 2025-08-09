36441fcce4eab142a2925d12de53aa8134533f74
Łukasz <247660@edu.p.lodz.pl>
2025-08-09 20:14:05 +0200

    - stworzenie repozytorium

---
+++ Arapp/src/test/java/engineer/arabski/ArappApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.arabski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
